0044dd32bf1d69bba6d3d56afc7fbc1e6b91f458
mirrus <sanserif8pt@gmail.com>
2025-09-03 09:16:52 +0200

    read in the API key from env

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, File, UploadFile,HTTPException
 from fastapi.params import Header
 from fastapi.responses import JSONResponse
 from typing import List
+from dotenv import load_dotenv
 import shutil
 import os
 
@@ -9,11 +10,12 @@ UPLOAD_DIR = "uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 app = FastAPI()
-API_KEY = "GPGE-TTY5-VBCD-A231-HPLK"
+API_KEY = os.getenv("API_KEY") #the key was changed from the previously hard-coded value
 @app.post("/upload")
 async def upload_image(file:UploadFile = File(...), x_api_key:str = Header(...)):
     if (x_api_key != API_KEY):
-        raise HTTPException(status_code=401, detail="Invalid API key")
+        raise HTTPException(status_code=401, detail="Invalid API key.")
+    
     if file.content_type not in ["image/jpeg", "image/png"]:
         raise HTTPException(status_code=400, detail="Invalid file type. Only JPEG and PNG are allowed.")
     
