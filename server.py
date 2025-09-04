c7fd5d711dba5e5a20a45a9463dffceab1c64f1f
mirrus <sanserif8pt@gmail.com>
2025-09-04 21:12:48 +0200

    save file with epoch time

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ from typing import List
 from dotenv import load_dotenv
 import shutil
 import os
+import time
+from pathlib import Path
 
 UPLOAD_DIR = "uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
@@ -12,15 +14,17 @@ load_dotenv()
 
 app = FastAPI()
 API_KEY = os.getenv("API_KEY") #the key was changed from the previously hard-coded value
-@app.post("/upload")
+
+@app.post("/esp32-api/upload")
 async def upload_image(file:UploadFile = File(...), x_api_key:str = Header(...)):
     if (x_api_key != API_KEY):
-        raise HTTPException(status_code=401, detail="Invalid API key.")
-    
+        raise HTTPException(status_code=401, detail="Invalid API key.")    
     if file.content_type not in ["image/jpeg", "image/png"]:
         raise HTTPException(status_code=400, detail="Invalid file type. Only JPEG and PNG are allowed.")
     
-    file_path = os.path.join(UPLOAD_DIR, file.filename)
+    old_path = Path(file.filename)
+    new_name = old_path.with_name(f"{time.time() * 1000}{old_path.suffix}")
+    file_path = os.path.join(UPLOAD_DIR, new_name)
     
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
